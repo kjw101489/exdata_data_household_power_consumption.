e2d3ac759f5a414e093990cb9b0e14da62a95d88
kjw101489 <cm_kjw@naver.com>
2017-01-22 08:43:33 +0900

    Update Rplot1.R

--- Rplot1.R
+++ Rplot1.R
@@ -1,3 +1,7 @@
-hist(data$Global_active_power, main = "Global Active Power", ylab = "Frequency", 
-    xlab = "Global Active Power (kilowatts)", col = "red", breaks = 13, ylim = c(0, 
-        1200), xlim = c(0, 6), xaxp = c(0, 6, 3))
+plot1 <- function() {
+        hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot1.png", width=480, height=480)
+        dev.off()
+        cat("Plot1.png has been saved in", getwd())
+}
+plot1()
