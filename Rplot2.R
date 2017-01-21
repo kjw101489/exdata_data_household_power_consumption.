52ccd1fb3faccf3db67855346100f3765a92bd7e
kjw101489 <cm_kjw@naver.com>
2017-01-22 08:44:17 +0900

    Update Rplot2.R

--- Rplot2.R
+++ Rplot2.R
@@ -1,1 +1,7 @@
-plot(data$Datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot1 <- function() {
+        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot2.png", width=480, height=480)
+        dev.off()
+        cat("plot2.png has been saved in", getwd())
+}
+plot1()
