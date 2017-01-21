e5e3a96d697cc50993297707e1b8487b3884fbb6
kjw101489 <cm_kjw@naver.com>
2017-01-22 08:45:01 +0900

    Update Rplot4.R

--- Rplot4.R
+++ Rplot4.R
@@ -1,22 +1,23 @@
-Plot 4par(mfrow = c(2, 2))
-
-# plot 1 (NW)
-plot(data$Datetime, data$Global_active_power, type = "l", ylab = "Global Active Power", 
-    xlab = "")
-
-# plot 2 (NE)
-plot(data$Datetime, data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
-
-# plot 3 (SW)
-plot(data$Datetime, data$Sub_metering_1, type = "l", ylab = "Energy sub metering", 
-    xlab = "", col = "black")
-points(data$Datetime, data$Sub_metering_2, type = "l", xlab = "", ylab = "Sub_metering_2", 
-    col = "red")
-points(data$Datetime, data$Sub_metering_3, type = "l", xlab = "", ylab = "Sub_metering_3", 
-    col = "blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", 
-    "Sub_metering_2", "Sub_metering_3"), bty = "n", )
-
-# plot 4 (SE)
-plot(data$Datetime, data$Global_reactive_power, type = "l", xlab = "datetime", 
-    ylab = "Global_reactive_power", ylim = c(0, 0.5))
+plot4 <- function() {
+        par(mfrow=c(2,2))
+        
+        ##PLOT 1
+        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+        ##PLOT 2
+        plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+        
+        ##PLOT 3
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col="red")
+        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
+        
+        #PLOT 4
+        plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+        
+        #OUTPUT
+        dev.copy(png, file="plot4.png", width=480, height=480)
+        dev.off()
+        cat("plot4.png has been saved in", getwd())
+}
+plot4()
